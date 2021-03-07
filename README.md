561b3902e18da8c2b904de1b3968c53751f0e4a8
ramsaycarslaw <ramsaycarslaw@icloud.com>
2021-03-07 16:46:47 +0000

    Finish lexing

---
+++ README.md
@@ -0,0 +1,3 @@
+# Parsing WolframAlpha style maths into LaTeX
+
+Simple tool to aid in the conversion from equations like `10*x^32` into  `$10 \cdot x^{32}$`
--- main.hs
+++ main.hs
@@ -10,7 +10,8 @@ data Equation = Constant Float
                 | Minus 
                 | LParen 
                 | RParen  
-                deriving (Show)
+                | Dot
+                deriving (Show, Eq)
 
 -- util function
 char2float :: Char -> Float
@@ -25,26 +26,48 @@ symbol '-' = Minus
 symbol '*' = Star
 symbol '/' = Slash
 symbol '^' = Carat
+symbol '.' = Dot
 symbol x = 
   if elem x ['0'..'9'] then
     (Constant (char2float x))
   else 
     (Variable x)
 
--- parse the input string
-parse :: String -> [Equation]
-parse xs = [symbol x | x <- xs, x /= ' ']
+-- tokenise the input string
+tokenise :: String -> [Equation]
+tokenise xs = [symbol x | x <- xs, x /= ' ']
 
 
+-- convert a list of type equation to a string
 showOp :: [Equation] -> String
 showOp [] = [] 
 showOp (o:os) = showo ++ ('\n' : showos)
    where showo = (show o) 
          showos = showOp os 
 
+-- print an equation
 printEq :: [Equation] -> IO ()
 printEq xs = putStr $ showOp xs
 
+-- returns true if an equation is a constant
+isConst :: Equation -> Bool
+isConst (Constant _) = True
+isConst _ = False
+
+getConst :: Equation -> Float
+getConst (Constant x) = x
+
+-- joins consecutive constants
+joinConst :: [Equation] -> [Equation]
+jointConst [] = []
+joinConst (x:xs) = 
+  if xs == [] then
+    [x]
+  else if isConst (head xs) && isConst x then
+      joinConst ([(Constant ((getConst x) * 10 + (getConst (head xs))))] ++ (tail xs))
+    else
+      [x] ++ joinConst xs
+         
 -- Add the latex enivornment around a string
 dollars :: String -> String
 dollars xs = "$" ++ xs ++ "$"
@@ -56,6 +79,9 @@ main = do
   query <- getLine
 
   if query /= "q" then
-    printEq ( parse query )
+    do {
+    printEq ( joinConst (tokenise query )) ;
+    main
+    }
   else
     return ()
